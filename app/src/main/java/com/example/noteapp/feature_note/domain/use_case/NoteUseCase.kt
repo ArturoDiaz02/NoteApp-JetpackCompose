8a264b7447c53e3a5d5f935b0c7d63420b53c0df
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2022-12-06 00:08:13 -0500

    getNotes

--- app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUseCase.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUseCase.kt
@@ -3,5 +3,6 @@ package com.example.noteapp.feature_note.domain.use_case
 data class NoteUseCase(
     val addNote: AddNote,
     val deleteNote: DeleteNote,
-    val getNoteId: GetNoteId
+    val getNoteId: GetNoteId,
+    val getNotes: GetNotes
 )
