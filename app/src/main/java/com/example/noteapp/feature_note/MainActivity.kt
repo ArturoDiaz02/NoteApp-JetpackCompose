0ec96b22151c6925836f4ecf3376deada8d42664
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2022-12-05 23:14:32 -0500

    Init

---
+++ app/src/main/java/com/example/noteapp/feature_note/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.noteapp.feature_note
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
